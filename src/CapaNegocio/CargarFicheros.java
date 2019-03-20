eea51849b1e706efc3a5fab6f2050fff60e81e6a
VicGoico <33021153+WilsonIsAlive@users.noreply.github.com>
2019-03-20 01:24:50 +0100

    Subiendo principio del proyecto

---
+++ src/CapaNegocio/CargarFicheros.java
@@ -0,0 +1,28 @@
+package CapaNegocio;
+
+public class CargarFicheros {
+	private String nombreFichero1 = "";
+	private String nombreFichero2 = "";
+	
+	private String[] nombresColumna;
+	private String[] datos;
+	
+	public CargarFicheros(){
+		
+		// Exito
+		if(cargarFichero1() && cargarFichero2()){
+			
+		}
+	}
+
+	private boolean cargarFichero2() {
+		
+		return false;
+	}
+
+	private boolean cargarFichero1() {
+		
+		return false;
+	}
+	
+}
---
+++ src/Main/main.java
@@ -0,0 +1,11 @@
+package Main;
+
+import CapaNegocio.CargarFicheros;
+
+public class main {
+
+	public static void main(String[] args) {
+		CargarFicheros carga = new CargarFicheros();
+	}
+
+}
